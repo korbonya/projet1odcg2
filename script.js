1e224dcca79aee9c6a8fc745023ccd85618c875e
Mamadou Alpha <mamadoualpha618@gmail.com>
2023-09-05 17:17:59 +0000

    adding submenu

---
+++ script.js
@@ -0,0 +1,6 @@
+let submenu = document.querySelector(".sub-menu");
+let formation = document.querySelector("#formation");
+
+formation.addEventListener("click", function () {
+    submenu.classList.toggle("active");
+});
